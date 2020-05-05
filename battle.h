01ed72f2b23030d47334e0a0e0306414f7dc3693
mattheww16 <63286693+mattheww16@users.noreply.github.com>
2020-05-05 20:21:17 +0700

    Update battle.h

--- battle.h
+++ battle.h
@@ -1,53 +1,91 @@
 #include <iostream>
+#include <string>
 #include "character.h"
 #ifndef BATTLE_H
 #define BATTLE_H 
 using namespace std;
 
-void battle(enemy & test, player & me){
+void recurringEffect(int counter, enemy & creep, player & player1) {
+
+}
+
+void battle(enemy & creep, player & player1){
     int counter = 0;
     bool fleeing = false;
-    cout << "You have encountered a " << test.name << "!" << endl;
-    test.printCharacter();
-    cout << "Health: " << test.health << endl;
+    cout << "You have encountered a " << creep.name << "!" << endl;
+    creep.printCharacter();
+    cout << "Health: " << creep.health << endl;
+    int enemyHealth = creep.health;
+    int playerHealth = player1.health;
+    int playerMana = player1.mana;
 
-    while((test.health != 0) || (fleeing == false)) {
+    while((enemyHealth > 0) || (fleeing == false) || playerHealth > 0) {
         if(counter % 2 == 0) {  // player's move
+            player1.healthManaBar(playerHealth, playerMana);
             int input;
             cout << "Your turn, choose any of the following actions!" << endl;
             cout << "1. Attack" << endl;
             cout << "2. Use item" << endl;
             cout << "3. Use spells" << endl;
-            cout << "4. Flee" << endl;
             cin >> input;
-            while ((input < 1) || (input > 4)) {
+            while ((input != 1) || (input != 2) || (input != 3)) {
                 cout << "Invalid input! Please try again" << endl;
                 cin >> input;
             }
-            switch (input) {
-                case 1:
-                    test.decreaseHealth(me.damage);
-                    cout << "You dealt " << me.damage << " damage to " << test.name << "!" << endl;
-                    break;
-                case 2:
-
-                    break;
-                case 3:
-                    
-                    break;
-                case 4:
-                    fleeing = true;
-                    break;
+            if(input == 1) {
+                enemyHealth -= player1.attack(creep.name);
+            }
+            else if(input == 2) {
+                player1.showItems();
+                cout << "Type \"back\" to go back and choose another action" << endl;
+                string itemOption;
+                getline(cin, itemOption);
+                if(itemOption == "back") {
+                    counter -= 1;
+                }
+                else{
+                    player1.useItems(itemOption, playerHealth, enemyHealth, counter);
+                }
+            }
+            else if(input == 3) {
+                player1.showSpells();
+                cout << "Type the potion name to use them or type \"back\" to go back and choose another action!" << endl;
+                string spellOption;
+                getline(cin, spellOption);
+                if(spellOption == "back") {
+                    counter -= 1;
+                }
+                else{
+                    player1.useSpells(spellOption);
+                }
             }
             counter += 1;
         }
         else if (counter % 2 == 1) { // enemy's move
-            test.attack();
+            creep.healthBar(enemyHealth);
+            playerHealth -= creep.attack();
             counter += 1;
         }
     }
-    if(fleeing == false) {
-
+    if(enemyHealth <= 0) {
+        cout << "Great work! You Defeated the " << creep.name << endl;
+        if(player1.xp < 20 * 1000) {
+            player1.xp += creep.xp;
+            player1.coin += creep.coin;
+            cout << "You received " << creep.xp << "XP and " << creep.coin << "gold!" << endl;
+        }
+        else{
+            player1.coin += creep.coin;
+            cout << "You received " << creep.coin << "gold!" << endl;
+        }
+        player1.showLevel();
     }
+    else if(playerHealth <= 0) {
+        cout << "You died! Rest up and try again another time!" << endl;
+    }
+    else if(fleeing ==  true) {
+        cout << "Whew, Got out of there safely!" << endl;
+    }
+    
 }
 #endif
