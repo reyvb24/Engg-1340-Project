bd2ac194e8218e954004c95b5ffcbdacafcb3b4d
reyvb24 <61707645+reyvb24@users.noreply.github.com>
2020-05-09 20:53:37 +0700

    Update potions.h
    add comments

--- potions.h
+++ potions.h
@@ -8,27 +8,27 @@ using namespace std;
 const int amount_of_health_potions = 3;
 const int amount_of_attack_potions = 3;
 
-struct health_potion {
+struct health_potion {//struct for health potion items
     string name;
     int health;
     int price;
 };
 
-struct attack_potion {
+struct attack_potion {//struct for attack potion items
     string name;
     int attack;
     int price;
     string descriptions;
 };
 
-struct hybrid_potion {
+struct hybrid_potion {//struct for hybrid potion (only 1 item)
     string name;
     int attack;
     int health;
     int price;
     string descriptions;
 };
-
+//assign values to the structs above
 struct health_potion potions[amount_of_health_potions] = {{ "magic elixir", 10, 15}, { "magic cure", 20, 30}, { "super magical potion", 80, 100}};
 
 struct attack_potion attack_elixir[amount_of_health_potions] = {{ "stun attack", 50, 15, "skip your opponent's next turn"}, 
